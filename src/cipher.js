533463c824f1a34bdbf0ec2b7c48fbcda731872a
Teresa Oyarce <t.oyarce05@gmail.com>
2021-05-19 15:19:40 -0400

    camino largo

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,45 @@
-const cipher = {
-  // ...
-};
+const abc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
+const cipher = {
+    encode: (texto1, offset) => {
+        var resultado = "";
+
+        offset = (offset % 26) % 26;
+        if (texto1) {
+            for (var i = 0; i < texto1.length; i++) {
+                if (abc.indexOf(texto1[i] != -1)) {
+                    var posicion = (abc.indexOf(texto1[i]) + offset) % 26; /*Traeme del abc,el resultado de la posicion del texto + el offset, y residuo de 26*/
+                    resultado += abc[posicion];
+                } else {
+                    resultado += texto1[i];
+                }
+            }
+        }
+
+        return resultado;
+    },
+
+    decode: (texto1, offset) => {
+        var resultado = "";
+
+        offset = (offset % 26) % 26;
+        /*Esta formula se ocupa para calcular el desplazamiento,
+               El 26 es para que al llegar a Z,vuelva a A */
+
+
+        /* Bucle para que vaya recorriendo el texto y vaya cifrando*/
+        if (texto1) {
+            for (var i = 0; i < texto1.length; i++) {
+                if (abc.indexOf(texto1[i] != -1)) {
+                    var posicion = (abc.indexOf(texto1[i]) - offset) % 26;
+                    resultado += abc[posicion];
+                } else {
+                    resultado += texto1[i];
+                }
+            }
+        }
+
+        return resultado;
+    },
+};
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 import cipher from './cipher.js';
 
+cifrar.addEventListener("click", () => {
+
+    var texto1 = document.getElementById("texto1").value;
+    var mayuscula = texto1.toUpperCase();
+    var offset = document.getElementById("rango").value;
+
+
+
+
+    var mensajecifrado = cipher.encode(mayuscula, offset);
+
+    document.getElementById("texto2").value = mensajecifrado;
+});
+
+descifrar.addEventListener("click", () => {
+
+    var texto1 = document.getElementById("texto1").value;
+    var mayuscula = texto1.toUpperCase();
+    var offset = document.getElementById("rango").value;
+
+    var mensajecifrado = cipher.decode(mayuscula, offset);
+
+    document.getElementById("texto2").value = mensajecifrado;
+});
+
+
+
+
+
+
+
 console.log(cipher);
