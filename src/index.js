53ef9d5325232baddf020efbcf0beb420dcae0e6
Teresa Oyarce <t.oyarce05@gmail.com>
2021-05-19 16:08:41 -0400

    cipher camino corto

--- src/index.js
+++ src/index.js
@@ -2,27 +2,31 @@ import cipher from './cipher.js';
 
 cifrar.addEventListener("click", () => {
 
+
     var texto1 = document.getElementById("texto1").value;
-    var mayuscula = texto1.toUpperCase();
+    var texto2 = document.getElementById("texto2");
+    texto2.value = "";
     var offset = document.getElementById("rango").value;
 
 
 
 
-    var mensajecifrado = cipher.encode(mayuscula, offset);
+    var mensajecifrado = cipher.encode(texto1, offset);
 
-    document.getElementById("texto2").value = mensajecifrado;
+
 });
 
 descifrar.addEventListener("click", () => {
 
     var texto1 = document.getElementById("texto1").value;
-    var mayuscula = texto1.toUpperCase();
+
     var offset = document.getElementById("rango").value;
+    var texto2 = document.getElementById("texto2");
+    texto2.value = "";
 
-    var mensajecifrado = cipher.decode(mayuscula, offset);
+    var mensajecifrado = cipher.decode(texto1, offset);
 
-    document.getElementById("texto2").value = mensajecifrado;
+
 });
 
 
