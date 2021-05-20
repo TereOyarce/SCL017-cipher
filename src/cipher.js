6465a3d9492570d2b34f610d1bf12f635db531da
Teresa Oyarce <t.oyarce05@gmail.com>
2021-05-20 16:43:48 -0400

    Agregado el valor de espacio

--- src/cipher.js
+++ src/cipher.js
@@ -5,16 +5,20 @@ const cipher = {
         var resultado = "";
 
         offset = (offset % 26) % 26;
+
         if (texto1) {
-            for (var i = 0; i < texto1.length; i++)
+
+            for (var i = 0; i < texto1.length; i++) {
+                if (texto1[i] == " ") { texto2.value += " "; } /*Este If busca si hay un espacio,si es así lo trae*/
                 for (var n = 0; n < abc.length; n++) {
-                    if (texto1[i].toUpperCase() == abc[n]) { /*El valor de i es a el valor de n? Si no es así,pasa la siguiente iteración*/
+                    if (texto1[i].toUpperCase() == abc[n]) { /*El valor de i es = a el valor de n? Si no es así,pasa la siguiente iteración*/
                         var formula = (n + offset) % 26;
+
                         texto2.value += abc[formula]; /* Texto2= a Texto2 + el resultado de la formula*/
                     }
                 }
+            }
         }
-
 
 
 
@@ -31,16 +35,19 @@ const cipher = {
 
         /* Bucle para que vaya recorriendo el texto y vaya cifrando*/
         if (texto1) {
-            for (var i = 0; i < texto1.length; i++)
+            for (var i = 0; i < texto1.length; i++) {
+                if (texto1[i] == " ") { texto2.value += " "; }
                 for (var n = 0; n < abc.length; n++) {
                     if (texto1[i].toUpperCase() == abc[n]) {
                         var formula = (n - offset) % 26;
                         texto2.value += abc[formula];
                     }
                 }
+            }
         }
 
         return resultado;
+
     },
 };
 export default cipher;
