5c9e6221d26a9b1eefa454d43fe8ffab45231887
Teresa Oyarce <t.oyarce05@gmail.com>
2021-05-24 16:53:48 -0400

    Versión estable ,test corriendo

--- src/cipher.js
+++ src/cipher.js
@@ -1,34 +1,33 @@
 const abc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var texto2 = document.getElementById("texto2");
+
 
 const cipher = {
     encode: (texto1, offset) => {
-
+        var resultado = '';
         offset = (offset % 26) % 26;
 
         if (texto1) {
 
             for (var i = 0; i < texto1.length; i++) {
-                if (texto1[i] == " ") { texto2.value += " "; } /*Este If busca si hay un espacio,si es así lo trae*/
+                if (texto1[i] == " ") { resultado += " "; } /*Este If busca si hay un espacio,si es así lo trae*/
                 for (var n = 0; n < abc.length; n++) {
                     if (texto1[i].toUpperCase() == abc[n]) { /*El valor de i es = a el valor de n? Si no es así,pasa la siguiente iteración*/
                         var formula = (n + offset) % 26;
-
-                        texto2.value += abc[formula]; /* Texto2= a Texto2 + el resultado de la formula*/
+                        resultado += abc[formula]; /* Texto2= a Texto2 + el resultado de la formula*/
                     }
                 }
             }
         }
 
 
-
+        return resultado;
 
     },
 
     decode: (texto1, offset) => {
+        var resultado = '';
 
-
-        offset = (offset % 26) % 26;
+        offset = ((offset % 26) - 26) % 26;
         /*Esta formula se ocupa para calcular el desplazamiento,
                El 26 es para que al llegar a Z,vuelva a A */
 
@@ -36,17 +35,17 @@ const cipher = {
         /* Bucle para que vaya recorriendo el texto y vaya cifrando*/
         if (texto1) {
             for (var i = 0; i < texto1.length; i++) {
-                if (texto1[i] == " ") { texto2.value += " "; }
+                if (texto1[i] == " ") { resultado += " "; }
                 for (var n = 0; n < abc.length; n++) {
                     if (texto1[i].toUpperCase() == abc[n]) {
                         var formula = (n - offset) % 26;
-                        texto2.value += abc[formula];
+                        resultado += abc[formula];
                     }
                 }
             }
         }
 
-
+        return resultado;
 
     },
 };
