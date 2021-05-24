a97fce0d2de4da3232c259c1824df8da5eb24500
Teresa Oyarce <t.oyarce05@gmail.com>
2021-05-24 11:28:15 -0400

    Actualizado js

--- src/index.js
+++ src/index.js
@@ -1,38 +1,36 @@
 import cipher from './cipher.js';
 
+var texto1 = "";
+var texto2 = "";
+var offset = "";
+var cifrar = document.getElementById("cifrar");
+var descifrar = document.getElementById("descifrar");
+
 cifrar.addEventListener("click", () => {
 
 
-    var texto1 = document.getElementById("texto1").value;
-    var texto2 = document.getElementById("texto2");
+    texto1 = document.getElementById("texto1").value;
+    texto2 = document.getElementById("texto2");
     texto2.value = "";
-    var offset = document.getElementById("rango").value;
+    offset = document.getElementById("rango").value;
 
 
 
 
-    var mensajecifrado = cipher.encode(texto1, offset);
+    cipher.encode(texto1, offset);
 
 
 });
 
 descifrar.addEventListener("click", () => {
 
-    var texto1 = document.getElementById("texto1").value;
+    texto1 = document.getElementById("texto1").value;
 
-    var offset = document.getElementById("rango").value;
-    var texto2 = document.getElementById("texto2");
+    offset = document.getElementById("rango").value;
+    texto2 = document.getElementById("texto2");
     texto2.value = "";
 
-    var mensajecifrado = cipher.decode(texto1, offset);
+    cipher.decode(texto1, offset);
 
 
 });
-
-
-
-
-
-
-
-console.log(cipher);
