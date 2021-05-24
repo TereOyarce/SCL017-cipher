92f15ed9a8a75e8b3d9be52ff51d55f21ee43adb
Teresa Oyarce <t.oyarce05@gmail.com>
2021-05-24 17:07:29 -0400

    Editado lorem ipsum

--- test/cipher.spec.js
+++ test/cipher.spec.js
@@ -19,10 +19,10 @@ describe('cipher', () => {
         });
 
         it('should throw TypeError when invoked with wrong argument types', () => {
-            // expect(() => cipher.encode('', 0)).toThrow(TypeError);
-            // expect(() => cipher.encode(null, 0)).toThrow(TypeError);
-            // expect(() => cipher.encode(null, [])).toThrow(TypeError);
-            // expect(() => cipher.encode(0, 0)).toThrow(TypeError);
+            expect(() => cipher.encode('', 0)).toThrow(TypeError);
+            expect(() => cipher.encode(null, 0)).toThrow(TypeError);
+            expect(() => cipher.encode(null, [])).toThrow(TypeError);
+            expect(() => cipher.encode(0, 0)).toThrow(TypeError);
         });
 
         it('should return "HIJKLMNOPQRSTUVWXYZABCDEFG" for "ABCDEFGHIJKLMNOPQRSTUVWXYZ" with offset 33', () => {
@@ -65,10 +65,10 @@ describe('cipher', () => {
         });
 
         it('should throw TypeError when invoked with wrong argument types', () => {
-            // expect(() => cipher.decode('', 0)).toThrow(TypeError);
-            // expect(() => cipher.decode(null, 0)).toThrow(TypeError);
-            // expect(() => cipher.decode(null, [])).toThrow(TypeError);
-            // expect(() => cipher.decode(0, 0)).toThrow(TypeError);
+            expect(() => cipher.decode('', 0)).toThrow(TypeError);
+            expect(() => cipher.decode(null, 0)).toThrow(TypeError);
+            expect(() => cipher.decode(null, [])).toThrow(TypeError);
+            expect(() => cipher.decode(0, 0)).toThrow(TypeError);
         });
 
         it('should return "ABCDEFGHIJKLMNOPQRSTUVWXYZ" for "HIJKLMNOPQRSTUVWXYZABCDEFG" with offset 33', () => {
